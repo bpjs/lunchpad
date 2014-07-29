a7b40cae4d9f1d9d7fdb5f111c9b457e816fbfcd
pcrglennon <pcrglennon@gmail.com>
2014-07-29 17:09:59 -0400

    Better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,18 @@ sophies = flatiron.restaurants.create(name: "Sophie's", category: "Cuban", yelp_
 chipotle = flatiron.restaurants.create(name: "Chipotle", category: "Mexican", yelp_url: "http://www.yelp.com/biz/chipotle-mexican-grill-new-york-30", address: "2 Broadway")
 flavors = flatiron.restaurants.create(name: "Flavors", category: "Salad/Sandwiches", yelp_url: "http://www.yelp.com/biz/flavors-new-york", address: "27 Whitehall St")
 
-brandon = flatiron.members.create(name: "Brandon Hopkins")
-peter = flatiron.members.create(name: "Peter Glennons")
-jess = flatiron.members.create(name: "Jess Rudder")
-simon = flatiron.members.create(name: "Simon Seroussi")
+brandon = flatiron.members.create(name: "Brandon Hopkins", email: "bh@foo.com", password: "password1234", password_confirmation: "password1234")
+peter = flatiron.members.create(name: "Peter Glennon", email: "pg@foo.com", password: "password1234", password_confirmation: "password1234")
+jess = flatiron.members.create(name: "Jess Rudder", email: "jr@foo.com", password: "password1234", password_confirmation: "password1234")
+simon = flatiron.members.create(name: "Simon Seroussi", email: "ss@foo.com", password: "password1234", password_confirmation: "password1234")
 
+brandon.reviews.create(restaurant: sophies, rating: 5.0, text: "I would give it a 5.")
+brandon.reviews.create(restaurant: chipotle, rating: 3.5, text: "I like it alright.")
+brandon.reviews.create(restaurant: flavors, rating: 4.0, text: "I like Flavors.  They have quinoa.")
+
+brandon.reviews.create(restaurant: sophies, rating: 4.2, text: "A little sketched out by the meat.")
+brandon.reviews.create(restaurant: flavors, rating: 3.8, text: "My fallback lunch place.")
+
+jess.reviews.create(restaurant: chipotle, rating: 4.0, text: "Sometimes you just gotta go to Chipotle.")
+
+simon.reviews.create(restaurant: chipotle, rating: 4.0, text: "They're super annoying (what's it called?).  Oh, the sneeze guard.  Assholes.")
