2780905e7233ef1a09a4aa1228f7ab35da8b2f31
pcrglennon <pcrglennon@gmail.com>
2014-07-30 10:44:35 -0400

    Review partial + review form.  Handle review creation in controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,15 @@ class ReviewsController < ApplicationController
   before_action :get_review, only: [:edit, :update, :destroy]
 
   def create
-
+    @review = Review.create(review_params)
+    @review.member = current_member
+    if @review.save
+      redirect_to(restaurant_path(@review.restaurant))
+    else
+      # TODO - make json!
+      flash[:alert] = "You messed up!!"
+      redirect_to(root_path)
+    end
   end
 
   def edit
