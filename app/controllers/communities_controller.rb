2bab142214816c22233995742c1101e8454b0914
pcrglennon <pcrglennon@gmail.com>
2014-08-03 19:26:13 -0400

    Move Restaurant search field, change default # of restaurants per page to 4

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,7 +2,7 @@ class CommunitiesController < ApplicationController
 
   def show
     @community = Community.find(params[:id])
-    @restaurants = Restaurant.by_average_rating_for_community(@community.id).paginate(:per_page => 5, :page => params[:page])
+    @restaurants = Restaurant.by_average_rating_for_community(@community.id).paginate(:per_page => 4, :page => params[:page])
   end
 
 end
