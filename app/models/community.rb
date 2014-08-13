53a589c9e4b5d043c84e834d8f7d5c6cd05d697b
pcrglennon <pcrglennon@gmail.com>
2014-08-12 20:48:42 -0400

    Validate that geolocation was successful

--- app/models/community.rb
+++ app/models/community.rb
@@ -4,16 +4,22 @@ class Community < ActiveRecord::Base
   has_many :groups
 
   validates_presence_of :name
-  validates :latitude, presence: true, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
-  validates :longitude, presence: true, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+  validate :validate_latitude_and_longitude
 
   def existing_restaurant(yelp_url)
     restaurants.where(yelp_url: yelp_url).first
   end
 
   def address=(address)
-    binding.pry
     self.latitude, self.longitude = Geocoder.coordinates(address)
+
   end
 
+  private
+
+    def validate_latitude_and_longitude
+      if self.latitude == nil || self.longitude == nil
+        self.errors.add(:base, "Not a valid address")
+      end
+    end
 end
