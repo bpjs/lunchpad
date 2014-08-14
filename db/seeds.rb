8b80e5263f1721b586df60328c8e4327403de409
pcrglennon <pcrglennon@gmail.com>
2014-08-14 13:24:30 -0400

    Change Peter reviews, remove Simon's review for Chipotle (for science fair), and move people around in lunch groups for easier less buggy demoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
+
+# The Flatiron School + Gluten-Free Flatiron
 
 flatiron = Community.create(name: "Flatiron School", latitude: 40.705397, longitude: -74.013924)
 
@@ -10,6 +10,11 @@ sophies = flatiron.restaurants.create(name: "Sophie's", category: "Cuban", yelp_
 chipotle = flatiron.restaurants.create(name: "Chipotle", category: "Mexican", yelp_url: "http://www.yelp.com/biz/chipotle-mexican-grill-new-york-30", address: "2 Broadway", latitude: 40.704499, longitude: -74.012825, image_url: 'http://s3-media4.fl.yelpcdn.com/bphoto/udIMvXLKSXt4NIQRSgY5_g/l.jpg')
 flavors = flatiron.restaurants.create(name: "Flavors", category: "Salad/Sandwiches", yelp_url: "http://www.yelp.com/biz/flavors-new-york", address: "27 Whitehall St", latitude: 40.704649, longitude: -74.011790, image_url: 'http://s3-media3.fl.yelpcdn.com/bphoto/cB6ieKowle5xPudpEW5jkg/l.jpg')
 yips = flatiron.restaurants.create(name: "Yips Restaurant", category: "Chinese", yelp_url: "http://www.yelp.com/biz/yips-restaurant-new-york", address: "18 Beaver St", latitude: 40.704994, longitude: -74.012389, image_url: 'http://s3-media4.fl.yelpcdn.com/bphoto/HJH5xf_bN2iPcxoQjYwn2w/l.jpg')
+
+sophies_group = Group.create(time: "2000-01-01 12:40:00", community_id: 1, restaurant_id: 1)
+chipotle_group = Group.create(time: "2000-01-01 13:00:00", community_id: 1, restaurant_id: 2)
+flavors_group = Group.create(time: "2000-01-01 12:00:00", community_id: 1, restaurant_id: 3)
+yips_group = Group.create(time: "2000-01-01 12:20:00", community_id: 1, restaurant_id: 4)
 
 brandon = flatiron.members.create(name: "Brandon Hopkins", email: "bh@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 1)
 peter = flatiron.members.create(name: "Peter Glennon", email: "pg@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 1)
@@ -19,9 +24,9 @@ chris = flatiron.members.create(name: "Chris Callahan", email: "chris@foo.com", 
 michael = flatiron.members.create(name: "Michael Loker", email: "michael@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 3)
 jacob = flatiron.members.create(name: "Jacob Barriaeult", email: "jacob@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 3)
 amy = flatiron.members.create(name: "Amy Johnson", email: "amy@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 2)
-ashley = flatiron.members.create(name: "Ashley Blewer", email: "ashley@foo.com", password: "password1234", password_confirmation: "password1234")
-rebecca = flatiron.members.create(name: "Rebecca Greenblatt", email: "rebecca@foo.com", password: "password1234", password_confirmation: "password1234")
-vanessa = flatiron.members.create(name: "Vanessa Dean", email: "vanessa@foo.com", password: "password1234", password_confirmation: "password1234")
+ashley = flatiron.members.create(name: "Ashley Blewer", email: "ashley@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 3)
+rebecca = flatiron.members.create(name: "Rebecca Greenblatt", email: "rebecca@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 3)
+vanessa = flatiron.members.create(name: "Vanessa Dean", email: "vanessa@foo.com", password: "password1234", password_confirmation: "password1234", group_id: 4)
 
 gf_at_flatiron = Community.create(name: "Gluten-Free Flatiron", latitude: flatiron.latitude, longitude: flatiron.longitude)
 
@@ -30,25 +35,21 @@ dig_inn = gf_at_flatiron.restaurants.create(name: "Dig Inn Seasonal Market", cat
 gf_at_flatiron.members << [peter, ashley, rebecca]
 gf_at_flatiron.save
 
-sophies_group = Group.create(time: "2000-01-01 12:40:00", community_id: 1, restaurant_id: 1)
-chipotle_group = Group.create(time: "2000-01-01 13:00:00", community_id: 1, restaurant_id: 2)
-flavors_group = Group.create(time: "2000-01-01 12:00:00", community_id: 1, restaurant_id: 3)
-yips_group = Group.create(time: "2000-01-01 12:20:00", community_id: 1, restaurant_id: 1)
-
 brandon.reviews.create(restaurant: sophies, rating: 5, text: "Awesome Cuban food in huge portions--it takes a lot of willpower not to eat an entire meal in one sitting, rather than doing the responsible thing and saving half for dinner.")
 brandon.reviews.create(restaurant: chipotle, rating: 3, text: "Classic standby for when you leave the school without figuring out what you want for lunch.")
 brandon.reviews.create(restaurant: flavors, rating: 4, text: "Lots of variety. Their specialty is their quinoa bowls with all kinds of mixins.")
 
-peter.reviews.create(restaurant: sophies, rating: 4, text: "A little sketched out by the meat.")
-peter.reviews.create(restaurant: flavors, rating: 3, text: "My fallback lunch place.")
+peter.reviews.create(restaurant: sophies, rating: 4, text: "I love this place, especially when they have Ropa Vieja.  But I always feel a little for about 30 minutes after eating there.")
+peter.reviews.create(restaurant: chipotle, rating: 4, text: "It's a Chipotle, do you really need a review?  Just go before 11:45 or after 2:30 unless you like waiting in line for ~15 minutes.")
+peter.reviews.create(restaurant: flavors, rating: 3, text: "Fallback lunch place.  You know what you're getting: it's not fantastic, but consistently pretty good.")
 
 jess.reviews.create(restaurant: chipotle, rating: 4, text: "The line is intimidating at lunch time, but you can get through it in about 15 minutes.  Definitely worth it when you're craving some spicy food.")
 jess.reviews.create(restaurant: yips, rating: 2, text: "There were not a lot of options for vegetarians.  I did enjoy the free can of Mountain Dew included with the meal though.")
 jess.reviews.create(restaurant: flavors, rating: 3, text: "The ingredients seem nice, but every time I go I manage to make a horrible mix. I should stick to prefab salads.")
 
-simon.reviews.create(restaurant: chipotle, rating: 4, text: "They're super annoying (what's it called?).  Oh, the sneeze guard.  Assholes.")
-
 chris.reviews.create(restaurant: yips, rating: 4, text: "Great value, gotta love the ribs with black bean sauce. The two ladies who work there are super nice! :)")
+
+# The Bluth Company
 
 the_bluth_company = Community.create(name: "The Bluth Company", latitude: 33.6220717, longitude: -117.9249228)
 
